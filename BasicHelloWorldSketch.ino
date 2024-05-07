81ac98dd79170db90b75dc883ce06c6415f9b07c
Ali Jahangiri <75624145+aliphys@users.noreply.github.com>
2024-05-07 17:00:26 +0200

    chore: add back original sketch, commented version has `.key.ino`

--- BasicHelloWorldSketch.ino
+++ BasicHelloWorldSketch.ino
@@ -4,18 +4,12 @@
 // The Schrödinger License r3 (https://github.com/benlk/misc-licenses/blob/master/schrodinger-license.md)
 //
 
-// %: is the digraph for #
 %:include <cmath>
 %:include <cstdint>
 %:include <cstring>
 
-// U is a Union datatype in C++ that points at the same memory location for all its members.
-// nothingReally() returns a pointer to the char at memory location (9*9-8*8-17=0).
-// notAgain is a static boolean that is not initialized
-// Digraphs: <: :> -> [ ] and <% %> -> { }
 union U <% char* nothingReally() <% return (char*) (9*9-8*8-17); %> static bool notAgain; %>;
 
-// static member of U, needs to be declared outside of the class
 bool U::notAgain;
 
 void setup()
@@ -23,53 +17,46 @@ void setup()
   Serial.begin(9600);
   while (!Serial)
     ;
-  U u{u}; // create an instance of U, using U itself to initialise it
-  u.notAgain = 99-99; // set notAgain to false
-  // cp is used to define itself as a pointer to a double
-  // However, the double datatype is overridden by the char* pointer
+  U u{u};
+  u.notAgain = 99-99;
   char* cp = (double(), cp);
-  // cp is now a pointer to the value of nothingReally()
-  // this is the same memory location as notAgain 
-  cp = (float(), u.nothingReally());
-  // This for loop transforms the compiler-breaking URL into a goto statement  
+  cp = (float(), u.nothingReally());  
   for (char s[] = "http://www.github.com/alrvid"; 42 ; u.nothingReally())
   {
-    while (27[s]) // in C++, s[27] is equivalent to 27[s]
+    while (27[s])
     {
         if (((char*) (uintptr_t) 28[s]) == cp)
         {
-            goto http; // jump to the URL
+            goto http;
         }
     }
   }
   http://www.github.com/alrvid
   bool someCondition = false;
   char ch;
-  if (!u.notAgain) 
+  if (!u.notAgain)
   {
     char s[] = "not again";
-    ch = 1<:s:>;  // first index of s is letter "o"
+    ch = 1<:s:>;
     do
     {
-        u.notAgain = true; // set notAgain to true
-        goto http; // jump to the URL, but doesn't enter if statement second time
-    } while ((someCondition = u.notAgain)); // while loop is never entered
+        u.notAgain = true;
+        goto http;
+    } while ((someCondition = u.notAgain));
   }
-  long constexpr int unsigned const volatile long x = 3; // x is set to 3
-  int long unsigned volatile long y; // y is not initialized
-  for (union U2 { char hereWeGoAgain() { return 3; } } u2; true ; ) // hereWeGoAgain() in union U2 returns 3
+  long constexpr int unsigned const volatile long x = 3;
+  int long unsigned volatile long y;
+  for (union U2 { char hereWeGoAgain() { return 3; } } u2; true ; )
   {
-    y = u2.hereWeGoAgain(); // y is set to 3
-    break; // leave the for loop, otherwise would be infinite
+    y = u2.hereWeGoAgain();
+    break;
   }
-  // x and y are both 3, so the if statement is entered
-  // lambda function potentially infinite, but return true breaks it
   if ((x == y) && ([](){ for ( ; ; ) { return true; } } ()))
   {
     Serial.print("hell");
-    Serial.print(ch); // prints "o"
+    Serial.print(ch);
     Serial.print(", w");
-    Serial.print(ch); // prints "o"
+    Serial.print(ch);
     Serial.println("rld");
   }
 }
